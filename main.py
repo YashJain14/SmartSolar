f031a8e68afb61fe95dd3499a03881555e29cf1e
YashJain14 <67069635+YashJain14@users.noreply.github.com>
2023-02-10 11:37:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from sense_hat import SenseHat
+from time import sleep
+from random import uniform
+
+sense = SenseHat()
+userchoice = 0
+userinput = 0.0
+sourceintensity = 0.0
+modulator = 0.0
+roomintensity = 0.0
+artificialintensity = 0.0
+print("1: Change room light intensity\n2: Display current light data")
+
+while True:
+    sourceintensity = uniform(0.0,1000.0)
+    try:
+      userchoice = int(input("Enter your choice: "))
+    except:
+      print("Invalid input, enter 1 or 2 only")
+      continue
+    if ((userchoice != 1) and (userchoice != 2)):
+      print("Invalid input, enter 1 or 2 only")
+      continue
+    
+    if (userchoice == 1):
+      userinput = float(input("Set room light intensity: "))
+      
+    if (userinput <= sourceintensity):
+      artificialintensity = 0.0
+      modulator = sourceintensity - userinput
+      roomintensity = sourceintensity - modulator
+    else:
+      modulator = 0.0
+      artificialintensity = userinput - sourceintensity
+      roomintensity = sourceintensity + artificialintensity
+    
+    if (userchoice == 2):
+      print("Room light intensity: {a}; Artificial light intensity: {b}; External light intensity: {c}; Modulator: {d}".format(a = roomintensity, b = artificialintensity, c = sourceintensity, d = modulator))
